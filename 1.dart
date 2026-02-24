3703971fd0b126f710294e6ab466b0a3f7be4394
iamdhruv17 <dsinghal7834@gmail.com>
2026-02-24 11:43:06 +0530

    24-02-2026

--- 1.dart
+++ 1.dart
@@ -1,3 +1,43 @@
+// void main(){
+//   print("Hello World");
+// }
+
+import 'dart:io';
+
 void main(){
-  print("Hello World");
+  // String a=stdin.readLineSync()!;
+  // String?a;
+  // a ??= "Dhruv";
+  // print(a);
+
+  // String ? s=stdin.readLineSync(); 
+
+	print(""); 
+  print("CASE 5");
+	// CASE 5: Custom Exception
+	try {
+		depositMoney(-200);
+	} catch (e) {
+		print(e);
+	} finally {
+		// Code
+	}
 }
+
+class DepositException implements Exception {
+	String errorMessage() {
+		return "You cannot enter amount less than 0";
+	}
+
+  String toString() {
+    return errorMessage();
+  } 
+}
+
+void depositMoney(int amount) {
+	if (amount < 0) {
+		throw new DepositException();
+	}
+}
+
+
